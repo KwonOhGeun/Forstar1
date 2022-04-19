bce03ebe4a16f12dc550fe966c9c77c1000c2680
KwonOhGeun <ohgeun72@naver.com>
2022-04-19 10:39:00 +0900

    Initial commit

---
+++ src/main/java/Forstar1.java
@@ -0,0 +1,16 @@
+public class Forstar1 {
+    public static void main(String[] args) {
+        //3행 4열 별찍기 문제입니다.
+        int i, j;
+
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 4; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+
+    }
+}
+
